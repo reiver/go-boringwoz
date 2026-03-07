5eec88373cd5659228e56b28234fde26f04dda30
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-07 14:16:25 -0800

    all

--- all.go
+++ all.go
@@ -5,54 +5,10 @@ var (
 	allSurnames   []string
 )
 
-var (
-	maxNameLength int
-)
-
 func init() {
 	allAdjectives = append(allAdjectives, wozAdjectives[:]...)
 	allAdjectives = append(allAdjectives, vanAdjectives[:]...)
 
 	allSurnames = append(allSurnames, wozSurnames[:]...)
 	allSurnames = append(allSurnames, vanSurnames[:]...)
-
-	{
-		{
-			var max int
-
-			for _, str := range allAdjectives {
-				// We calculate the string length using len() because all characters in the 'adjectives' and 'surnames' are ASCII (and thus 1 byte long).
-				length := len(str)
-				if max < length {
-					max = length
-				}
-			}
-
-			maxNameLength += max
-		}
-
-		maxNameLength += len("_")
-
-		{
-			var max int
-
-			for _, str := range allSurnames {
-				// We calculate the string length using len() because all characters in the 'adjectives' and 'surnames' are ASCII (and thus 1 byte long).
-				length := len(str)
-				if max < length {
-					max = length
-				}
-			}
-
-			maxNameLength += max
-		}
-
-		maxNameLength += len("_")
-
-		maxNameLength += 2
-
-		if maxNameLength < 32 {
-			maxNameLength = 32
-		}
-	}
 }
